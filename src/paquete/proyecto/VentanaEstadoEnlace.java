2fd572ae52769604672030e392c7481bcbe7e484
Almar1999 <79382177+Almar1999@users.noreply.github.com>
2021-02-21 17:30:07 -0500

    ModificacionDom21

--- src/paquete/proyecto/VentanaEstadoEnlace.java
+++ src/paquete/proyecto/VentanaEstadoEnlace.java
@@ -1,4 +1,230 @@
-/*//GEN-LINE:variables
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:variables
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtCantEnlace = new javax.swing.JTextField();
+        txtCantRouter = new javax.swing.JTextField();
+        txtRouterRaiz = new javax.swing.JTextField();
+        btnNuevo = new javax.swing.JButton();
+        btnIngreso_arista = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable = new javax.swing.JTable();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        btnEscenario1 = new javax.swing.JButton();
+        btnEscenario2 = new javax.swing.JButton();
+        btnEscenario3 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 153));
+        jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jLabel1.setFont(new java.awt.Font("Consolas", 0, 24)); // NOI18N
+        jLabel1.setText("ENRUTAMIENTO ESTADO-ENLACE");
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jLabel2.setText("Cantidad de routers en la topología:");
+
+        jLabel3.setText("Router raíz:");
+
+        jLabel4.setText("Cantidad de enlaces:");
+
+        txtCantRouter.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtCantRouterActionPerformed(evt);
+            }
+        });
+
+        btnNuevo.setText("Nuevo");
+        btnNuevo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNuevoActionPerformed(evt);
+            }
+        });
+
+        btnIngreso_arista.setText("Botón ingreso de aristas");
+        btnIngreso_arista.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIngreso_aristaActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtRouterRaiz, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(12, 12, 12)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(txtCantRouter, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel4)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtCantEnlace, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(btnIngreso_arista)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnNuevo)))
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel4)
+                    .addComponent(txtCantRouter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtCantEnlace, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtRouterRaiz, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnNuevo)
+                    .addComponent(btnIngreso_arista))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                " Router", "Router Anterior", "Peso"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable);
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel5.setText("Escenarios");
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel6.setText("Tabla de enrutamiento");
+
+        btnEscenario1.setText("Escenario 1");
+        btnEscenario1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEscenario1ActionPerformed(evt);
+            }
+        });
+
+        btnEscenario2.setText("Escenario 2");
+        btnEscenario2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEscenario2ActionPerformed(evt);
+            }
+        });
+
+        btnEscenario3.setText("Escenario 3");
+        btnEscenario3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEscenario3ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(39, 39, 39))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel6)
+                                .addGap(160, 160, 160))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 303, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(btnEscenario1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnEscenario2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 10, Short.MAX_VALUE)
+                                .addComponent(btnEscenario3)))
+                        .addContainerGap())))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 362, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(110, 110, 110))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(13, 13, 13)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel5))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                        .addContainerGap())
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnEscenario1)
+                            .addComponent(btnEscenario2)
+                            .addComponent(btnEscenario3))
+                        .addContainerGap(148, Short.MAX_VALUE))))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>                        /*//GEN-END:variables
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -268,6 +494,7 @@ public class VentanaEstadoEnlace extends javax.swing.JFrame {
 
     private void btnIngreso_aristaActionPerformed(java.awt.event.ActionEvent evt) {                                                  
         // TODO add your handling code here:
+        // TODO add your handling code here:
         
         //Validacion de ingreso
         if (this.txtCantEnlace.getText().isEmpty() || this.txtCantRouter.getText().isEmpty()||
@@ -280,6 +507,16 @@ public class VentanaEstadoEnlace extends javax.swing.JFrame {
             //Ingreso de la cantidad de router que hay en la topologia
             int cantRouter=Integer.parseInt(this.txtCantRouter.getText());
             int tablaEstadoEnlace[][]=new int[cantRouter][cantRouter]; //Se formara la tabla en base a la cantidad de router que se ingrese
+            
+            //Ingreso solo del router raiz
+            int routRaiz=Integer.parseInt(this.txtRouterRaiz.getText());
+            for (int i=0; i<cantRouter; i++)
+            {   for (int j=0; j<cantRouter; j++)
+                {
+                    tablaEstadoEnlace[i][j]=1000;
+                }
+            }
+
             //Ingreso de la cantidad de enlaces
             int cantEnlace=Integer.parseInt(this.txtCantEnlace.getText());
             for(int i=0;i<cantEnlace;i++)
@@ -289,31 +526,23 @@ public class VentanaEstadoEnlace extends javax.swing.JFrame {
                 //observa en la tipologia
                 IngresoDeArista(tablaEstadoEnlace);
             }
-            //Ingreso solo del router raiz
-            int routRaiz=Integer.parseInt(this.txtRouterRaiz.getText());
-            for (int i=0; i<cantRouter; i++)
-            {   for (int j=0; j<cantRouter; j++)
-                {
-                    tablaEstadoEnlace[i][j]=1000;
-                }
-            }
+            
             
             int [][]tablaGeneral= TablaGeneralT(cantRouter,routRaiz);
             for(int i=0;i<cantRouter;i++){
                 algoritmoDijkstra(cantRouter,tablaGeneral,tablaEstadoEnlace);
             }
             
-            
-            //for (int i=0; i < tablaGeneral.length; i++){
-            //    for(int j=0; j<3; j++){
-            //        System.out.print (tablaGeneral[i][j]+"\t\t\t");
-            //    }
-                
-            //}
-                    
+            jTextArea.append("Router\tPeso \tRouter anterior\n\n");
+            for (int i=0; i < tablaGeneral.length; i++) {
+                System.out.println("\n\n");
+                for (int j=0; j<3; j++) 
+                {
+                    this.jTextArea.append(tablaGeneral[i][j]+ "\t\t\t");
+                }
+            }
+                               
             //fuente: https://yoandroide.xyz/grafos-implementacion-algoritmo-dijkstra-en-java
-            //Invocamos a la funcion tabla
-            llenadoTabla();
         }
     }                                                 
 
@@ -353,20 +582,22 @@ public class VentanaEstadoEnlace extends javax.swing.JFrame {
     }                                        
 
     //Metodos
-    private void IngresoDeArista(int[][]tablaEstadoEnlace ){
+    private static void IngresoDeArista(int[][]tablaEstadoEnlace ){
         //En base a los ingresos de del peso, numero del router inicial y final,
         //se almacenaran en la tablaArista
         int Inicio= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero del router inicial:"));
         int Final= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero del router final:"));
         int Peso= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el peso:"));
         tablaEstadoEnlace[Inicio][Final]=Peso;
+        
     }
     //Metodo auxiliar para algoritmoDijsktra
     private int routerConMenorRuta(int n,int[][]tabla){
         int posicionR=0;
+        int menor =1000;
         for(int i=0; i<n; i++){
-            if(tabla[i][3]!=1 && tabla[i][1]<= 1000){
-                tabla[i][1]=1000;
+            if(tabla[i][3]!=1 && tabla[i][1]<= menor){
+                menor = tabla[i][1];
                 posicionR=i;   
             }
         }
@@ -377,8 +608,10 @@ public class VentanaEstadoEnlace extends javax.swing.JFrame {
         int posicion=routerConMenorRuta(n,tabla);
         tabla[posicion][3]=1;
         for(int i=0;i<n;i++){
-            if(tablaEstadoEnlace[posicion][i]!=1000){
-                if(tablaEstadoEnlace[posicion][i]+tabla[posicion][1]<tabla[i][1]){
+            if(tablaEstadoEnlace[posicion][i]!=1000)
+            {
+                if(tablaEstadoEnlace[posicion][i]+tabla[posicion][1]<tabla[i][1])
+                {
                     tabla[i][1]= tablaEstadoEnlace[posicion][i] + tabla[posicion][1];
                     tabla[i][2]=posicion;
                 }  
@@ -386,21 +619,17 @@ public class VentanaEstadoEnlace extends javax.swing.JFrame {
         }
     }
     
-    private int[][] TablaGeneralT(int routRaiz,int cantRouter){
+    private int[][] TablaGeneralT(int n,int rInic){
         
-        int tabla[][]=new int[cantRouter][4];
-        for(int i=0;i<cantRouter;i++){
+        int tabla[][]=new int[n][4];
+        for(int i=0;i<n;i++){
             tabla[i][0]=i;
             tabla[i][1]=1000;
             tabla[i][2]=-1;        
             tabla[i][3]=0;
         }
-        tabla[routRaiz][1]=0;
+        tabla[rInic][1]=0;
         return tabla;   
-    }
-    
-    private void llenadoTabla(){
-
     }
             
     /**
